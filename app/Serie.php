4b89f61d6aa8d23c2881720daede069fcf4f2934
cristiano7988 <cristianooliveiramorales@gmail.com>
2020-12-20 09:59:12 -0300

    Criação das tabelas  Temporadas e Episodios e seus relacionamentos

--- app/Serie.php
+++ app/Serie.php
@@ -2,10 +2,15 @@
 
 namespace App;
 
+use App\Models\Temporada;
 use Illuminate\Database\Eloquent\Model;
 
 class Serie extends Model
 {
     public $timestamps = false;
     protected $fillable = ['nome'];
+
+    public function temporadas() {
+        return $this->hasMany(Temporada::class);
+    }
 }
