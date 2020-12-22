ff9bd91322a81818bbb643c34119c0ce0f6f323c
cristiano7988 <cristianooliveiramorales@gmail.com>
2020-12-21 22:16:24 -0300

    Exibe e esconde input para alterar nome da série

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -15,10 +15,19 @@ Séries
     @foreach ($series as $serie)
     <li class="list-group-item">
         <div class="row d-flex justify-content-between align-items-center m-1">
-            <div>
-                <span> {{ $serie->nome }}</span>
+            <div class="d-flex align-items-center">
+                <span id="nome-serie-{{$serie->id}}"> {{ $serie->nome }}</span>
+                
+                <div class="input-group w-100" hidden id="input-nome-serie-{{$serie->id}}">
+                    <input type="text" class="form-control" value="{{$serie->nome}}">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-primary" onclick="editarSerie({{ $serie->id }})"><i class="fas fa-check"></i></button>
+                        @csrf
+                    </div>
+                </div>
             </div>
             <div class="d-flex">
+                <button class="btn btn-primary mr-2" onclick="toggleInput({{ $serie->id }})"><i class="fas fa-edit"></i></button>
                 <a href="/series/{{ $serie->id }}/temporadas" class="btn btn-secondary">Temporadas</a>
                 <form method="post" action="series/remover/{{ $serie->id }}" onsubmit="return confirm('Tem certeza?')">
                     @csrf
@@ -30,3 +39,18 @@ Séries
     @endforeach
 </ul>
 @endsection
+
+<script>
+    function toggleInput(serieId) {
+        nomeEl = document.getElementById(`input-nome-serie-${serieId}`);
+        inputEl = document.getElementById(`nome-serie-${serieId}`); 
+        
+        inputEl.hasAttribute('hidden') ? (
+            inputEl.removeAttribute('hidden'),
+            nomeEl.hidden = true
+        ) : (
+            nomeEl.removeAttribute('hidden'),
+            inputEl.hidden = true
+        );
+    }
+</script>
